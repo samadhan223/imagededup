bea9942e8dd67a6196db1f4d28603f3bca5656ed
samadhan223 <104039323+samadhan223@users.noreply.github.com>
2023-08-25 20:14:09 +0530

    Update FastAPI_upload_file_task.py

--- FastAPI_upload_file_task.py
+++ FastAPI_upload_file_task.py
@@ -37,5 +37,4 @@ async def create_upload_file(file: UploadFile):
     #return {"info": f"file '{file.filename}' saved at '{file_location}'"}
 
 
-if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0')
+
